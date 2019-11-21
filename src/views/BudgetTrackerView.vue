d3e3aa82d1e619d8ba806d1a9fbbf31d54bbab5b
NikolaStojicic <nikolastojicicsd@gmail.com>
2019-11-21 01:26:32 +0100

    First day of sample app

---
+++ src/views/BudgetTrackerView.vue
@@ -0,0 +1,183 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" style="color: white">Budget app</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <router-link class="nav-link" to="/settings"
+              >Settings <span class="sr-only"></span
+            ></router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <div class="mt-1 container-fluid text-center">
+      <div class="row">
+        <div v-for="(item, name) in budget" :key="name" class="col-sm">
+          <button
+            type="button"
+            :class="`btn btn-${colorButton(name)} btn-lg btn-block`"
+          >
+            {{ item }}
+          </button>
+        </div>
+      </div>
+      <div class="row align-items-start">
+        <div class="col-sm">
+          <div class="alert alert-success item-container">
+            <div
+              v-for="(item, index) in incomeList"
+              :key="index"
+              class="alert alert-warning alert-dismissible fade show"
+            >
+              <strong>{{ item.opis }}</strong> {{ item.iznos }}
+              <button
+                @click="incomeList.splice(index, 1)"
+                type="button"
+                class="close"
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm">
+          <div class="alert alert-primary item-container">
+            <div class="input-group mb-3">
+              <input
+                v-model="inputs.opis"
+                type="text"
+                class="form-control"
+                placeholder="Opis"
+              />
+            </div>
+            <div class="input-group mb-3">
+              <input
+                v-model="inputs.iznos"
+                type="text"
+                class="form-control"
+                placeholder="Iznos"
+              />
+            </div>
+            <button
+              @click="insertNewBudget"
+              type="button"
+              class="btn btn-primary btn-block"
+            >
+              Unesi
+            </button>
+            <button
+              @click="clearFields"
+              type="button"
+              class="btn btn-secondary btn-block"
+            >
+              Poništi
+            </button>
+          </div>
+        </div>
+        <div class="col-sm">
+          <div class="alert alert-danger item-container">
+            <div
+              v-for="(item, index) in expensesList"
+              :key="index"
+              class="alert alert-warning alert-dismissible fade show"
+            >
+              <strong>{{ item.opis }}</strong> {{ item.iznos }}
+              <button
+                @click="expensesList.splice(index, 1)"
+                type="button"
+                class="close"
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    colorButton(name) {
+      return name === "income"
+        ? "success"
+        : name === "expenses"
+        ? "danger"
+        : "primary";
+    },
+    clearFields() {
+      this.inputs.opis = "";
+      this.inputs.iznos = "";
+    },
+    insertNewBudget() {
+      let iznos = parseInt(this.inputs.iznos);
+      if (iznos) {
+        if (iznos > 0) {
+          this.incomeList.push({
+            opis: this.inputs.opis,
+            iznos: iznos
+          });
+        } else {
+          this.expensesList.push({
+            opis: this.inputs.opis,
+            iznos: iznos
+          });
+        }
+      }
+      this.clearFields();
+    }
+  },
+  watch: {
+    incomeList() {
+      let sum = 0;
+      this.incomeList.forEach(el => {
+        sum += el.iznos;
+      });
+      this.budget.income = sum;
+
+      this.budget.balance = this.budget.income + this.budget.expenses;
+    },
+    expensesList() {
+      let sum = 0;
+      this.expensesList.forEach(el => {
+        sum += el.iznos;
+      });
+      this.budget.expenses = sum;
+
+      this.budget.balance = this.budget.income + this.budget.expenses;
+    }
+  },
+  data() {
+    return {
+      inputs: {
+        opis: "",
+        iznos: ""
+      },
+      budget: {
+        income: 0,
+        balance: 0,
+        expenses: 0
+      },
+      incomeList: [],
+      expensesList: []
+    };
+  }
+};
+</script>
+
+<style scoped></style>
---
+++ src/views/Settings.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit
+    earum aspernatur fuga dicta, ea vel debitis autem enim, rerum illo obcaecati
+    itaque eos nulla molestiae, incidunt odio temporibus natus recusandae
+    quibusdam deserunt ex! Suscipit vel assumenda est ratione quidem, nam
+    commodi numquam eaque recusandae quasi quos ex ipsa odio tempora tempore rem
+    dolores vitae neque id! Assumenda et explicabo unde aperiam molestias fugit
+    excepturi quaerat non voluptates aut delectus nemo consequuntur eos neque
+    error nulla nesciunt adipisci consectetur, dignissimos accusamus blanditiis?
+    Nostrum doloremque similique accusamus, veritatis dolorem aperiam
+    consectetur provident tempore aliquam voluptas molestiae repudiandae tempora
+    nesciunt delectus reprehenderit totam sequi odit distinctio quis
+    voluptatibus suscipit animi quae non itaque. Ullam mollitia ex veniam quia,
+    eligendi fuga excepturi autem, dicta voluptatum, reiciendis odit alias! Cum
+    impedit, ad voluptate asperiores magnam dicta ut debitis eveniet, sed eaque
+    ullam autem eius animi eum hic aliquam quod quidem sunt unde natus enim
+    voluptatem doloribus beatae a. Hic atque dicta dolor adipisci odio vel, nisi
+    doloribus, ut, quibusdam labore quas. Nobis magnam tempore quis debitis quo
+    asperiores perferendis. Veritatis nihil accusamus, sequi at a neque laborum.
+    Iste provident consequatur sed, deleniti enim, soluta dignissimos dolor,
+    alias mollitia accusantium minima officia? Quis nihil maiores eum vel omnis
+    adipisci! Veniam laborum voluptatem rerum perferendis, dolore deserunt
+    voluptatibus a nihil tempora quam ex voluptates amet corporis illo delectus!
+    At doloribus vitae, dolore tempora molestiae hic? Totam consequuntur,
+    numquam ab qui repellendus velit voluptatibus a, hic quidem fugiat
+    repudiandae sed quibusdam nisi quos fuga commodi excepturi optio deserunt
+    odit. Saepe molestiae amet sapiente veniam itaque! Laboriosam ipsum
+    obcaecati ad totam dignissimos, officiis reprehenderit itaque quas alias
+    hic, ab doloremque quidem illum rerum repudiandae? Ducimus culpa
+    voluptatibus quaerat ea veritatis dolore laboriosam ex, facere minus
+    similique iusto voluptatum debitis, necessitatibus earum id odit aperiam nam
+    consectetur, at delectus quibusdam.
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
